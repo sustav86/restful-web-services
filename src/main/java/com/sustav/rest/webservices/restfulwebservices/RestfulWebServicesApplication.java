306fb6c4b40febdfc7b63cdf36d4dcccc5f516bd
Anton Sustavov <sustav86@gmail.com>
2020-01-09 12:43:57 +0200

    add i18

--- src/main/java/com/sustav/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/sustav/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,8 +3,9 @@ package com.sustav.rest.webservices.restfulwebservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 import java.util.Locale;
 
@@ -17,10 +18,18 @@ public class RestfulWebServicesApplication {
 
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+//        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        AcceptHeaderLocaleResolver acceptHeaderLocaleResolver = new AcceptHeaderLocaleResolver();
+        acceptHeaderLocaleResolver.setDefaultLocale(Locale.US);
 
-        return sessionLocaleResolver;
+        return acceptHeaderLocaleResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource resourceBundleMessageSource() {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("messages");
+        return resourceBundleMessageSource;
     }
 
 }
--- src/main/java/com/sustav/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/sustav/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.sustav.rest.webservices.restfulwebservices.controller;
 
 import com.sustav.rest.webservices.restfulwebservices.bean.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
 //    @RequestMapping(path = "/hello-world", method = RequestMethod.GET)
     @GetMapping(path = "/hello-world")
@@ -24,8 +33,13 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello World Microservices from %s", name));
     }
 
-    @GetMapping(path = "/hello-world-interbalized")
-    public String helloWorldInternalized() {
-        return "Good morning";
+    @GetMapping(path = "/hello-world-internalized")
+    public String helloWorldInternalized(@RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
+    }
+
+    @GetMapping(path = "/hello-world-internalized-holder")
+    public String helloWorldInternalizedHolder() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
